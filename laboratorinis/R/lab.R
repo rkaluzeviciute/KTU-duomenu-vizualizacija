de574108dabe5ca4767e2ae4a7905c3e8f2e67e4
rkaluzeviciute <126467808+rkaluzeviciute@users.noreply.github.com>
2023-04-21 18:23:14 +0300

    pataisytas

--- laboratorinis/R/lab.R
+++ laboratorinis/R/lab.R
@@ -6,44 +6,28 @@ unzip("temp")
 raw <- read.csv("monthly-2022.csv", sep = ";")
 names(raw) <- c("code", "jarCode", "name", "municipality", "ecoActCode", "ecoActName", "month", "avgWage", "numInsured", "avgWage2", "numInsured2", "tax")
 
-codes <- raw %>%
-  group_by(ecoActCode) %>%
-  summarise(n = n()) %>%
-  arrange(desc(n)) %>%
-  na.omit() %>%
-  head(22)
-
-raw %>%
-  filter(ecoActCode %in% codes$ecoActCode) %>%
-  write_csv("lab_sodra.csv")
-
-raw_json <- head(raw) %>%
-  toJSON()
-
-write(raw_json, "lab_sodra.json")
-
 #1 uzduotis
-filtered_data=
-  lab_sodra%>%
+raw=
+  raw%>%
   filter(grepl("681000", ecoActCode))
 
 library(ggplot2)
-ggplot(data = filtered_data, aes(x = avgWage)) +
+graph=ggplot(data = raw, aes(x = avgWage)) +
   geom_histogram(color="red", fill="white")+
   labs(title = "Histogram of avgWage",
        x = "avgWage",
        y = "Count")
+summary(raw)
 
 # 2 uzduotis
-#data2 - kienvienos imones avgWage suskaiciuojam
-data2=       
-  filtered_data%>%
+data=       
+  raw%>%
   group_by(name)%>%
   summarise(avg_Wage=mean(avgWage, na.rm=TRUE))%>%
   arrange(desc(avg_Wage))  
 
-new_data = filtered_data %>% 
-  left_join(data2, by = "name") #prijungiam prie pradinio
+new_data=merge(raw, data, by = "name")
+new_data=filter(new_data, name %in% data$name)
 
 new_data=unique(new_data[, c("name", "avg_Wage")]) #paliekam nesikartojancius
 
@@ -52,7 +36,7 @@ new_data=new_data%>%   #randam 5 su didziausiu avgWage
   head(5)
 
 datamonths = 
-  filtered_data%>%
+  raw%>%
   filter(name %in% new_data$name)
 
 ggplot(datamonths, aes(x=month, y=avgWage, group=name, color=name))+
@@ -62,11 +46,12 @@ ggplot(datamonths, aes(x=month, y=avgWage, group=name, color=name))+
 #3 uzduotis
 insured_filtered=
   datamonths%>%
-  filter(month>=202204 & month<=202304)%>%
+  filter(month>=202201 & month<=202212)%>%
   group_by(name)%>%
-  summarise(insured=sum(numInsured))
+  summarise(max=max(numInsured))
 
-ggplot(insured_filtered, aes(x=reorder(name, -insured), y=insured))+
+
+ggplot(insured_filtered, aes(x=reorder(name, -max), y=max))+
   geom_bar(stat="identity", color="black", fill="lightblue",
            linetype="dashed")+
   labs(x = "Name", y = "numInsured", title = "numInsured by Name")
